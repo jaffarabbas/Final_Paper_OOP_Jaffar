1d2d4afb2465781de04ae068b6a034bdf7f192cc
Jaffar Abbas <gamaportal8@gmail.com>
2020-06-30 11:06:18 +0500

    30-6-2020

---
+++ src/jaffar/Main.java
@@ -0,0 +1,12 @@
+package jaffar;
+abstract class A{
+    abstract int add(int a,int b);
+}
+
+public class Main extends A {
+
+    public static void main(String[] args) {
+        x obj = new z();
+        obj.m();
+    }
+}
